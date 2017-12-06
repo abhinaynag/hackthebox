b73e54235181954fbf66779d8f104711dbd7fc49
Abhinay <abhinaynag@gmail.com>
2017-12-07 12:24:15 +1300

    Update active-recon.sh
    changed the deleting non compliant JSON lines based on text

--- scripts/active-recon.sh
+++ scripts/active-recon.sh
@@ -13,7 +13,5 @@ cat ../global_results/livehosts.txt | while read ip; do $(searchsploit -x --nmap
 
 cat ../global_results/livehosts.txt | while read ip; do $( searchsploit -x --nmap -j ../$ip/$ip-services.xml > ../$ip/$ip-exploits.json ); done
 
-cat ../global_output/livehosts.txt | while read ip ; do sed -i '1,2d' ../$ip/$ip-exploits.json ; done
-
-cat ../global_output/livehosts.txt | while read ip; do $(cat ../$ip/$ip-exploits.json | jq '.RESULTS[].Path' | sed 's/"//g' | while read link; do $(cp $link ../$ip/exploits/);done;); done
+cat ../global_output/livehosts.txt | while read ip; do $(cat ../$ip/$ip-exploits.json | sed '/Reading/d' |jq '.RESULTS[].Path' | sed 's/"//g' | while read link; do $(cp $link ../$ip/exploits/);done;); done
 
